f30b23156775293d26f20eef46702144704a10a6
Joris Blaak <joris@label305.com>
2014-05-12 12:39:35 +0200

    Changed way methods are called and added commenting per method

--- buffer.php
+++ buffer.php
@@ -1,36 +1,78 @@
-<?
+<?php
 	class BufferApp {
+
+		/**
+		 * Client id
+		 * @var string
+		 */
 		private $client_id;
+
+		/**
+		 * Client secret
+		 * @var string
+		 */
 		private $client_secret;
+
+		/**
+		 * Authorization code
+		 * @var string
+		 */
 		private $code;
+
+		/**
+		 * Access token
+		 * @var string
+		 */
 		private $access_token;
 		
+		/**
+		 * Callback url
+		 * @var string
+		 */
 		private $callback_url;
+
+		/**
+		 * Url used to authorize an application
+		 * @var string
+		 */
 		private $authorize_url = 'https://bufferapp.com/oauth2/authorize';
+
+		/**
+		 * Url used to resolve the access token
+		 * @var string
+		 */
 		private $access_token_url = 'https://api.bufferapp.com/1/oauth2/token.json';
+
+		/**
+		 * ApiV1 base url
+		 * @var string
+		 */
 		private $buffer_url = 'https://api.bufferapp.com/1';
 		
-		public $ok = false;
-		
+		/**
+		 * Supported endpoints
+		 * @var array
+		 */
 		private $endpoints = array(
 			'/user' => 'get',
-			
 			'/profiles' => 'get',
 			'/profiles/:id/schedules/update' => 'post',	// Array schedules [0][days][]=mon, [0][times][]=12:00
 			'/profiles/:id/updates/reorder' => 'post',	// Array order, int offset, bool utc
 			'/profiles/:id/updates/pending' => 'get',
 			'/profiles/:id/updates/sent' => 'get',
 			'/profiles/:id/schedules' => 'get',
-			'/profiles/:id' => 'get',
-			
-			'/updates/:id/update' => 'post',						// String text, Bool now, Array media ['link'], ['description'], ['picture'], Bool utc
-			'/updates/create' => 'post',								// String text, Array profile_ids, Aool shorten, Bool now, Array media ['link'], ['description'], ['picture']
+			'/profiles/:id' => 'get',		
+			'/updates/:id/update' => 'post',			// String text, Bool now, Array media ['link'], ['description'], ['picture'], Bool utc
+			'/updates/create' => 'post',				// String text, Array profile_ids, Aool shorten, Bool now, Array media ['link'], ['description'], ['picture']
 			'/updates/:id/destroy' => 'post',
 			'/updates/:id' => 'get',
-			
 			'/links/shares' => 'get',
 		);
 		
+		/**
+		 * List of BufferApp error codes
+		 * @var array
+		 */
 		public $errors = array(
 			'invalid-endpoint' => 'The endpoint you supplied does not appear to be valid.',
 
@@ -83,59 +125,70 @@
 			'400' => 'Media filesize out of acceptable range.',
 		);
 		
-		function __construct($client_id = '', $client_secret = '', $callback_url = '') {
-			if ($client_id) $this->set_client_id($client_id);
-			if ($client_secret) $this->set_client_secret($client_secret);
-			if ($callback_url) $this->set_callback_url($callback_url);
+		/**
+		 * Construct the BufferApp class
+		 * 
+		 * @param string $client_id     
+		 * @param string $client_secret 
+		 * @param string $callback_url  
+		 */
+		public function __construct($client_id, $client_secret, $callback_url) {
+			if($client_id) $this->setClientId($client_id);
+			if($client_secret) $this->setClientSecret($client_secret);
+			if($callback_url) $this->setCallBackUrl($callback_url);
 			
-			if ($_GET['code']) {
+			if($_GET['code']) {
 				$this->code = $_GET['code'];
 				$this->create_access_token_url();
 			}
-			
-			$this->retrieve_access_token();
 		}
 		
-		function go($endpoint = '', $data = '') {
-			if (in_array($endpoint, array_keys($this->endpoints))) {
+		/**
+		 * Do a request to an endpoint
+		 * @param  string $endpoint 
+		 * @param  string $data     
+		 * @return mixed
+		 */
+		public function go($endpoint, $data = array()) {
+			if(in_array($endpoint, array_keys($this->endpoints))) {
 				$done_endpoint = $endpoint;
 			} else {
 				$ok = false;
 				
+				//Replace the placeholders
 				foreach (array_keys($this->endpoints) as $done_endpoint) {
-					if (preg_match('/' . preg_replace('/(\:\w+)/i', '(\w+)', str_replace('/', '\/', $done_endpoint)) . '/i', $endpoint, $match)) {
+					if(preg_match('/' . preg_replace('/(\:\w+)/i', '(\w+)', str_replace('/', '\/', $done_endpoint)) . '/i', $endpoint, $match)) {
 						$ok = true;
 						break;
 					}
 				}
 				
-				if (!$ok) return $this->error('invalid-endpoint');
+				if(!$ok) return $this->error('invalid-endpoint');
 			}
 			
-			if (!$data || !is_array($data)) $data = array();
+			if(!$data || !is_array($data)) $data = array();
+
+			//Add the access token
 			$data['access_token'] = $this->access_token;
 			
 			$method = $this->endpoints[$done_endpoint]; //get() or post()
 			return $this->$method($this->buffer_url . $endpoint . '.json', $data);
 		}
 		
-		function store_access_token() {
-			$_SESSION['oauth']['buffer']['access_token'] = $this->access_token;
+		/**
+		 * Dispatch an error
+		 * @param  Unknown $error 
+		 * @return array
+		 */
+		public function error($error) {
+			return array('error' => $this->errors[$error]);
 		}
 		
-		function retrieve_access_token() {
-			$this->access_token = $_SESSION['oauth']['buffer']['access_token'];
-			
-			if ($this->access_token) {
-				$this->ok = true;
-			}
-		}
-		
-		function error($error) {
-			return (object) array('error' => $this->errors[$error]);
-		}
-		
-		function create_access_token_url() {
+		/**
+		 * Resolve the access token after the authentication
+		 * @return string|boolean false on error
+		 */
+		public function resolveAccessToken() {
 			$data = array(
 				'code' => $this->code,
 				'grant_type' => 'authorization_code',
@@ -144,19 +197,31 @@
 				'redirect_uri' => $this->callback_url,
 			);
 			
-			$obj = $this->post($this->access_token_url, $data);
-			$this->access_token = $obj->access_token;
+			$response = $this->post($this->access_token_url, $data);
+
+			if(!isset($response['access_token'])) {
+				return false;
+			}
+
+			$this->setAccessToken($response['access_token']);
 			
-			$this->store_access_token();
+			return $this->access_token;
 		}
 		
-		function req($url = '', $data = '', $post = true) {
-			if (!$url) return false;
-			if (!$data || !is_array($data)) $data = array();
+		/**
+		 * Perform a request as the BufferApp api likes it
+		 * @param  string  $url  
+		 * @param  string  $data 
+		 * @param  boolean $post 
+		 * @return string
+		 */
+		public function request($url = '', $data = '', $post = true) {
+			if(!$url) return false;
+			if(!$data || !is_array($data)) $data = array();
 						
 			$options = array(CURLOPT_RETURNTRANSFER => true, CURLOPT_HEADER => false);
 			
-			if ($post) {
+			if($post) {
 				$options += array(
 					CURLOPT_POST => $post,
 					CURLOPT_POSTFIELDS => $data
@@ -170,37 +235,76 @@
 			$rs = curl_exec($ch);
 			
 			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			if ($code >= 400) {
+			if($code >= 400) {
 				return $this->error($code);
 			}
 			
-			return json_decode($rs);
+			return json_decode($rs, true);
 		}
 		
-		function get($url = '', $data = '') {
-			return $this->req($url, $data, false);
+		/**
+		 * Perform a get request
+		 * @param  string $url  
+		 * @param  string $data 
+		 * @return mixed   
+		 */
+		public function get($url = '', $data = '') {
+			return $this->request($url, $data, false);
 		}
 		
-		function post($url = '', $data = '') {
-			return $this->req($url, $data, true);
+		/**
+		 * Perform a post request
+		 * @param  string $url  
+		 * @param  string $data 
+		 * @return mixed      
+		 */
+		public function post($url = '', $data = '') {
+			return $this->request($url, $data, true);
+		}
+			
+		/**
+		 * Fetch url to redirect client to for the OAuth2 process
+		 * @return String
+		 */
+		public function getLoginUrl() {
+			$query = array(
+				'client_id' => $this->client_id,
+				'redirect_uri' => $this->callback_url,
+				'response_type' => 'code'
+				);
+
+			return $this->authorize_url . '?' . http_build_query($query);
+		}
+
+		/**
+		 * Setter for the access token
+		 * @param string $access_token 
+		 */
+		public function setAccessToken($access_token) {
+			$this->access_token = $access_token;
 		}
 		
-		function get_login_url() {
-			return $this->authorize_url . '?'
-    		. 'client_id=' . $this->client_id
-    		. '&redirect_uri=' . urlencode($this->callback_url)
-    		. '&response_type=code';
-		}
-		
-		function set_client_id($client_id) {
+		/**
+		 * Setter for the client id
+		 * @param string $client_id 
+		 */
+		public function setClientId($client_id) {
 			$this->client_id = $client_id;
 		}
 		
-		function set_client_secret($client_secret) {
+		/**
+		 * Setter for the client secret
+		 * @param string $client_secret 
+		 */
+		public function setClientSecret($client_secret) {
 			$this->client_secret = $client_secret;
 		}
 
-		function set_callback_url($callback_url) {
+		/**
+		 * Setter for the callback url
+		 * @param string $callback_url 
+		 */
+		public function setCallBackUrl($callback_url) {
 			$this->callback_url = $callback_url;
 		}
 	}
